66f607ce88a73c2a2c742331029c4c49355ce973
99-Knots <ninety9knots@outlook.com>
2024-04-26 12:34:55 +0200

    fixed taskbar icon

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import ctypes
 from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
 
 
 if __name__ == '__main__':
+    appID = 'PDFStitcher-v0.2'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appID)
     app = QApplication(sys.argv)
     win = MainWindow()
     win.show()
